42ca3a1f38fb144a184c45b4b3cce6b9f43f75cf
Daniel Whiffing <danielwhiffing@gmail.com>
2024-07-21 13:17:00 -0400

    Adds env to next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
+const isProd = process.env.NODE_ENV === 'production'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
-  assetPrefix: './'
+  output: isProd ? 'export' : undefined,
+  assetPrefix: isProd ? './' : undefined
 };
 
 export default nextConfig;
